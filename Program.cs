7b5caee1bd6d89bc7bd3d332a9a3ea63ff1dc4ee
Emir Topaloglu <nonotelite@gmail.com>
2023-01-23 14:19:31 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myproject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Please enter threshold number: ");
+      int maxNumber = int.Parse(Console.ReadLine());
+
+      int[] randomThinks = new int[1];
+
+      int Fonk1(int a, int b)
+      {
+        int toplam = a + b;
+
+        if ((toplam) > maxNumber)
+        {
+          Console.WriteLine(toplam);
+          return toplam;
+        }
+        else
+        {
+          Console.WriteLine(toplam);
+          return (Fonk1(b, toplam));
+        }
+      }
+      Fonk1(0, 1);
+    }
+  }
+}
+
